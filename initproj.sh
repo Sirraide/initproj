1e0ee755e1c35a2c17dd3a61a5562a4987cc1cf0
Sirraide <aeternalmail@gmail.com>
2023-04-11 15:22:14 +0200

    Replace - with _ in header guard

--- initproj.sh
+++ initproj.sh
@@ -33,7 +33,7 @@ cp "$dir"/CMakeLists.txt .
 ## Replace the project name.
 sed -i "s/hello/$1/g" CMakeLists.txt
 sed -i "s/hello/$1/g" .gitignore
-sed -i "s/HELLO_/$(echo "$1" | tr a-z A-Z)_/g" src/utils.hh
+sed -i "s/HELLO_/$(echo "$1" | tr a-z A-Z | tr '-' '_')_/g" src/utils.hh
 find .idea -type f -exec grep -q hello {} \; -print0 | xargs -0 sed -i "s/hello/$1/g"
 
 ## Initialise a git repository and add fmt and clopts as submodules.
